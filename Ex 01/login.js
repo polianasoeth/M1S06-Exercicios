deb08d2a3689911b0eef87b1b5354fe76f6b788d
Poliana Soeth <polianacristinasoeth16@gmail.com>
2023-07-05 19:47:30 -0300

    Iniciando Ex01

---
+++ Ex 01/login.js
@@ -0,0 +1,26 @@
+function fazerLogin() {
+    const email = document.getElementById('campo-email').value
+    const senha = document.getElementById('campo-senha').value
+
+    document.getElementById('campo-email').classList.remove("input-error")
+    document.getElementById('error-email').hidden = true
+
+    document.getElementById('campo-senha').classList.remove("input-error")
+    document.getElementById('error-senha').hidden = true
+
+
+    if (email === "") {
+        document.getElementById('campo-email').classList.add('input-error')
+        document.getElementById('error-email').hidden = false
+        document.getElementById('error-email').innerText = "Digite um email válido"
+
+
+    } else (senha.length < 9) {
+        document.getElementById('campo-senha').classList.add('input-error')
+        document.getElementById('error-senha').hidden = false
+        document.getElementById('error-senha').innerText = "A senha deve no minimo 8 caracteres"
+    }
+
+
+}
+
